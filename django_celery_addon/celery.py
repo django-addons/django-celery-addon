7048bc502414ca0cd555a19ecf70d9adcb9c5cc7
Stefan Foulis <stefan.foulis@sherpany.com>
2018-03-20 22:46:30 +0100

    adapt celery.py

--- django_celery_addon/celery.py
+++ django_celery_addon/celery.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-import aldryn_django.startup
+import django_addon.startup
 
 from celery import Celery
 
 
-# `path` is not used anymore but still required
-aldryn_django.startup._setup(path=None)
+django_addon.startup._setup()
 
 from django.conf import settings  # noqa
 
-app = Celery('aldryn_celery')
+app = Celery('django_celery_addon')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
